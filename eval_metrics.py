614c7affbbc2acca377de8ab8ab6b21f963357d1
MHansinger <maximilian.hansinger@gmail.com>
2021-10-11 21:04:17 +0200

    Create eval_metrics.py

---
+++ eval_metrics.py
@@ -0,0 +1,19 @@
+import os
+import warnings
+import sys
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+
+import mlflow
+import mlflow.sklearn
+
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
