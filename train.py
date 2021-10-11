b955a07e48f1c7c0c31cefff6758902f846fe0b9
MHansinger <maximilian.hansinger@gmail.com>
2021-10-11 21:02:29 +0200

    Update train.py

--- train.py
+++ train.py
@@ -16,11 +16,11 @@ import mlflow
 import mlflow.sklearn
 
 
-def eval_metrics(actual, pred):
-    rmse = np.sqrt(mean_squared_error(actual, pred))
-    mae = mean_absolute_error(actual, pred)
-    r2 = r2_score(actual, pred)
-    return rmse, mae, r2
+# def eval_metrics(actual, pred):
+#     rmse = np.sqrt(mean_squared_error(actual, pred))
+#     mae = mean_absolute_error(actual, pred)
+#     r2 = r2_score(actual, pred)
+#     return rmse, mae, r2
 
 
 
